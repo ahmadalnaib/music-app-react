a1de7c98d7c055a4e8cbd1e01a8938cef832937b
ahmedhabeeb73 <alnaib888@gmail.com>
2020-11-10 10:57:29 +0100

    add our state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import './styles/app.scss';
 import Player from "./components/Player";
 import Song from "./components/Song";
+import data from './util';
 
 
 
 function App() {
+  // state
+  const[songs,setSongs]=useState(data());
+  const [currentSong,setCurrentSong]=useState(songs[0]);
   return (
     <div className="App">
       <h1>Music App</h1>
-      <Song/>
+      <Song currentSong={currentSong}/>
       <Player/>
     </div>
   );
